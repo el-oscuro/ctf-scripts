99e6324119313a72288295547bb83ae2172ecb2a
el-oscuro <20606182+el-oscuro@users.noreply.github.com>
2017-12-13 20:58:11 -0500

    haaaaaaaaaaaaands

--- mate.sh
+++ mate.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ `ls *.opvn | wc` != 1 ]; then
-	echo $0:  Create a separate directory for your ctf project and save your openvpn file in it
-	echo $0:  cd to that directy and run $0.  It will open up 5 tabs, one with your VPN, another with 
-	echo $0:  metasploit, and 3 addtional tabs
-	exit 1
+script=`basename $0`
+vpn_files=*.ovpn 
+if [ "`ls *.ovpn | wc -l`" != "1" ]; then
+  echo $script:  Create a separate directory for your ctf project and save your openvpn file in it
+  echo $script:  cd to that directy and run $0.  It will open up 5 tabs, one with your VPN, another with 
+  echo $script:  metasploit, and 3 addtional tabs
+exit 1
 fi
 
-OVPN=`ls *.opvn`
+OVPN=`ls *.ovpn`
 
-mate-terminal --tab --working-directory=$PWD --command=openvpn\ $OPVN
-mate-terminal --tab --working-directory=$PWD --command=msfconsole
+mate-terminal --tab --title="Open VPN" --working-directory=$PWD --command=openvpn\ $OVPN
+mate-terminal --tab --title="Metasploit" --working-directory=$PWD --command=msfconsole
 mate-terminal --tab --working-directory=$PWD 
 mate-terminal --tab --working-directory=$PWD 
 
