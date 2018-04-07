a6ce8dba383e80070af9cadd01a4a7e947ff399e
El Oscuro <mjwdams@gmail.com>
2018-04-06 20:39:20 -0400

    updated enumerate

--- enumerate.sh
+++ enumerate.sh
@@ -3,11 +3,11 @@ if [ "$1" == "" ]; then
   echo Usage: $0 IP
   exit 1
 fi
-#nmap $1
-#nmap -sS -v -A $1 | tee $1-nmap.txt
-#uniscan -u http://$1 -qweds
-#cp -v /usr/share/uniscan/report/$1.html uniscan-$1.html
-#nikto -host http://$1 -output $1-nikto.txt
+nmap $1
+nmap -sS -v -A $1 | tee $1-nmap.txt
+uniscan -u http://$1 -qweds
+cp -v /usr/share/uniscan/report/$1.html uniscan-$1.html
+nikto -host http://$1 -output $1-nikto.txt
 dirb http://$1/ -o $1-dirb.txt 
 
 
